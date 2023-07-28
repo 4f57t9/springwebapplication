26dd9cac8c7898db3a9ad50da79cf9a7ab8c2134
Üc207 Pr4 4f57t9 <myway5231@naver.com>
2023-07-28 14:04:07 +0900

    23.07.28 DAO(Data Access Object) @Mapper를 이용한 Dao @Mapper 와 mapper XML 연동

--- src/main/java/com/mycompany/springwebapp/controller/Ch13Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch13Controller.java
@@ -1,12 +1,17 @@
 package com.mycompany.springwebapp.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.springwebapp.dao.Ch13BoardDao;
 import com.mycompany.springwebapp.dao.Ch13BoardDaoOld;
+import com.mycompany.springwebapp.dao.Ch13BoardDaoOldImpl;
 import com.mycompany.springwebapp.dto.Ch13Board;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +23,9 @@ public class Ch13Controller {
 	@Resource
 	private Ch13BoardDaoOld boardDaoOld;
 	
+	@Autowired
+	private Ch13BoardDao boardDao;
+	
 	@RequestMapping("/content")
 	public String content() {
 		return "ch13/content";
@@ -28,29 +36,42 @@ public class Ch13Controller {
 		log.info("실행");
 		
 		Ch13Board board = new Ch13Board();
-		board.setBtitle("나는");
-		board.setBcontent("개허접이다");
+		board.setBtitle("아");
+		board.setBcontent("피곤하다");
 		board.setMid("user");
 		
-		boardDaoOld.insert(board);
+		// boardDaoOld.insert(board);
+		boardDao.insert(board);
+		
+		// 실제로 저장된bno : 
+		log.info("");
 		return "redirect:/ch13/content";
 	}
 	
 	@GetMapping("/getBoardList")
 	public String getBoardList() {
-		boardDaoOld.selectAll();
+		// List<Ch13Board> list = boardDaoOld.selectAll();
+		List<Ch13Board> list = boardDao.selectAll();
+		log.info(list.toString());
 		return "redirect:/ch13/content";
 	}
 	
 	@GetMapping("/updateBoard")
 	public String updateBoard() {
-		boardDaoOld.updateByBno();
+		// Ch13Board board = boardDaoOld.selectByBno(1);
+		Ch13Board board = boardDao.selectByBno(1);
+		board.setBtitle("ㄹㅇ");
+		board.setBcontent("ㅋㅋ");
+		
+		boardDaoOld.updateByBno(board);
 		return "redirect:/ch13/content";
 	}
 	
 	@GetMapping("/deleteBoard")
 	public String deleteBoard() {
-		boardDaoOld.deleteByBno();
+		int bno = 1;
+		// boardDaoOld.deleteByBno(bno);
+		boardDao.deleteByBno(bno);
 		return "redirect:/ch13/content";
 	}
 	
--- src/main/java/com/mycompany/springwebapp/dao/Ch13BoardDao.java
+++ src/main/java/com/mycompany/springwebapp/dao/Ch13BoardDao.java
@@ -1,10 +1,13 @@
 package com.mycompany.springwebapp.dao;
 
 import java.util.List;
-
+import org.apache.ibatis.annotations.Mapper;
 import com.mycompany.springwebapp.dto.Ch13Board;
 
-public interface Ch13BoardDaoOld {
+@Mapper
+public interface Ch13BoardDao {
+	// 추상 메소드 선언
+	// 메소드 이름이 board.xml의 mapper > insert id랑 같아야 한다
 	public int insert(Ch13Board board);
 	public List<Ch13Board> selectAll();
 	public Ch13Board selectByBno(int bno);
