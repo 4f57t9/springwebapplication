4adbcc245476041269caacb94e5430339c666dfe
Üc207 Pr4 4f57t9 <myway5231@naver.com>
2023-08-02 14:18:12 +0900

    23.08.01

--- src/main/java/com/mycompany/springwebapp/service/Ch15AccountServiceImpl.java
+++ src/main/java/com/mycompany/springwebapp/service/Ch15AccountServiceImpl.java
@@ -3,11 +3,17 @@ package com.mycompany.springwebapp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mycompany.springwebapp.dao.Ch15AccountDao;
 import com.mycompany.springwebapp.dto.Ch15Account;
+import com.mycompany.springwebapp.exception.Ch15NotFoundAccountException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class Ch15AccountServiceImpl implements Ch15AccountService {
 	@Autowired
 	private Ch15AccountDao accountDao;
@@ -24,7 +30,7 @@ public class Ch15AccountServiceImpl implements Ch15AccountService {
 		// 출금하기
 		Ch15Account fromAccount = accountDao.selectByAno(fromAno);
 		if(fromAccount == null) {
-			throw new RuntimeException("출금 계좌가 없음");
+			throw new Ch15NotFoundAccountException("출금 계좌가 없음");
 		} 
 		fromAccount.setBalance(fromAccount.getBalance() - amount);
 		accountDao.updateBalance(fromAccount); // DML
@@ -32,9 +38,9 @@ public class Ch15AccountServiceImpl implements Ch15AccountService {
 		// 입금하기
 		Ch15Account toAccount = accountDao.selectByAno(toAno);
 		if(toAccount == null) {
-			throw new RuntimeException("입금 계좌가 없음");
+			throw new Ch15NotFoundAccountException("입금 계좌가 없음");
 		} 
-		toAccount.setBalance(toAccount.getBalance() - amount);
+		toAccount.setBalance(toAccount.getBalance() + amount);
 		accountDao.updateBalance(toAccount);
 	}
 }
